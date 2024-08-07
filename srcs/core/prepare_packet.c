1abcff0f2f64ea1f21716e2457a194f159a502cb
intrusions <xeloronkali@gmail.com>
2024-08-07 11:32:43 +0200

    clean():

--- srcs/core/prepare_packet.c
+++ srcs/core/prepare_packet.c
@@ -8,9 +8,8 @@ void prepare_packet(t_data *data, t_packet *packet, u16 n_sequence)
     packet->hdr.un.echo.id = data->pid;
     packet->hdr.un.echo.sequence = n_sequence;
 
-    for (u32 j = 0; j < sizeof(packet->payload) - 1; j++) {
+    for (u32 j = 0; j < sizeof(packet->payload) - 1; j++)
         packet->payload[j] = j + '0';
-    }
     packet->payload[sizeof(packet->payload) - 1] = 0;
 
     packet->hdr.checksum = checksum(packet, sizeof(*packet));
--- srcs/core/recv_packet.c
+++ srcs/core/recv_packet.c
@@ -2,9 +2,9 @@
 
 void recv_packet(t_data *data, char *response, timeval *end_time)
 {
-    if (recvfrom(data->sockfd, response, MAX_PACKET_SIZE, 0, NULL, NULL) < 0) {
+    if (recvfrom(data->sockfd, response, MAX_PACKET_SIZE, 0, NULL, NULL) < 0)
         return ;
-    }
+    
     gettimeofday(end_time, NULL);
 
     if (data->flags & FLAG_D)
